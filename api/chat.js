82f3261c7c819ae665e51aa8688405433c7cf845
Dead-Pixel-cmd <68045356+Dead-Pixel-cmd@users.noreply.github.com>
2025-07-11 18:30:11 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -23,6 +23,10 @@ const loadResumeData = async () => {
 loadResumeData();
 
 export default async function handler(req, res) {
+res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
